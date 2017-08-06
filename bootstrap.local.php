b60168aa5d21018f742aeb894e74e6146ceb743c
Artyom Deryavka <deryavka@togethernetworks.com>
2017-08-06 15:23:33 +0000

    added local bootstrap logic

---
+++ bootstrap.local.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artyom
+ * Date: 06.08.2017
+ * Time: 15:15
+ */
+if (!isset($contentServiceBootstrapped) || $contentServiceBootstrapped === false) {
+    $routesPrefix = "content/";
+    require_once __DIR__ . "/config/routesMap.php";
+    require_once __DIR__ . "/config/servicesMap.php";
+    $routesPrefix = "";
+    $contentServiceBootstrapped = true;
+}
