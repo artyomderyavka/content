87cd52b18dbe3b5a8952626ea14751e541e69296
Artyom Deryavka <deryavka@togethernetworks.com>
2017-08-07 08:24:29 +0000

    added local bootstrap

--- bootstrap.local.php
+++ bootstrap.local.php
@@ -6,7 +6,7 @@
  * Time: 15:15
  */
 if (!isset($contentServiceBootstrapped) || $contentServiceBootstrapped === false) {
-    $routesPrefix = "content/";
+    $routesPrefix = "/content";
     require_once __DIR__ . "/config/routesMap.php";
     require_once __DIR__ . "/config/servicesMap.php";
     $routesPrefix = "";
